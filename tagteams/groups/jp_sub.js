5c29b3864ca8b6942d914c280146c260351e5f58
Kenta000 <49142511+Kenta000@users.noreply.github.com>
2023-04-06 15:28:58 +0800

    Remove CSS animation

--- tagteams/groups/jp_sub.js
+++ tagteams/groups/jp_sub.js
@@ -1086,7 +1086,7 @@ var jp_TagteamFocusCase = () => {
 
             icons.forEach((item, index) => {
                 item.addEventListener("mouseover", (e) => {
-                    focus(e.target, index);
+                    // focus(e.target, index);
                 });
                 item.addEventListener("mouseleave", (e) => {
                     icons.forEach((item) => {

3ca81f19c0b0a3c0d7b0eb4870e7361ca180e911
LinhTib <38345088+LinhTib@users.noreply.github.com>
2023-10-30 15:05:50 +0800

    Update wfm_helper.js
    update for Connect Calendar / Appointment and Waiting Queue Noti

--- wfm_helper.js
+++ wfm_helper.js
@@ -75,51 +75,108 @@
       }
     } else false
   }
-}
-
+};
 var country = {
-  COUNTRY:"NA",
-  JP:"JP",
-  KR:"KO",
-  ID:"ID",
-  VN:"VI",
-  CN:"ZH"
-}
-
-var show = function(e){
-    var a = e.target.value;
-    if (a == "COUNTRY") {
-      document.querySelectorAll(".rotate").forEach(function(e){e.parentNode.click()});
-    } else {
-    //console.log("hide all");
-    document.querySelectorAll(".rotate").forEach(function(e){e.parentNode.click()});
-    //console.log("open JP");
-    document.querySelectorAll('[class="pool-name"]').forEach(function (name) {
-      name.parentNode.parentNode.parentNode.parentNode.style.display='none';
+  COUNTRY: "NA",
+  TH: "TH",
+  JP: "JP",
+  KR: "KO",
+  ID: "ID",
+  VN: "VI",
+  CN: "ZH"
+};
+var show = function(e) {
+  var a = e.target.value;
+  if (a == "COUNTRY") {
+    document.querySelectorAll(".rotate").forEach(function(e) {
+      e.parentNode.click()
+    });
+  } else {
+    document.querySelectorAll(".rotate").forEach(function(e) {
+      e.parentNode.click()
+    });
+    document.querySelectorAll('[class="pool-name"]').forEach(function(name) {
+      name.parentNode.parentNode.parentNode.parentNode.style.display = 'none';
       name.innerText.includes(country[a]) && name.click();
     });
-    //console.log("open all calendar");
-    document.querySelectorAll('[class="availability-name"]').forEach(function (name) {
-      name.parentNode.parentNode.parentNode.parentNode.style.display='none';
-      name.previousSibling.classList.contains("rotate")&&name.click();
-      name.innerText.includes("KL - S&T - "+a)&&!name.previousSibling.classList.contains("rotate")&&name.click();
+    document.querySelectorAll('[class="availability-name"]').forEach(function(name) {
+      name.parentNode.parentNode.parentNode.parentNode.style.display = 'none';
+      name.previousSibling.classList.contains("rotate") && name.click();
+      if (country[a] == 'TH') {
+        name.innerText.toUpperCase().includes("TH") && name.click();
+      } else if (country[a] == 'VI') {
+        name.innerText.toUpperCase().includes("VN") && name.click(); 
+} else {
+        name.innerText.includes("KL - S&T - " + a) && !name.previousSibling.classList.contains("rotate") && name.click();
+      }
     });
   }
-    setTimeout(showAll,1000);
-}
-
-ori = document.querySelector(".timezone");
-sel = document.createElement("select");
+  setTimeout(showAll, 1000);
+};
+let ori = document.querySelector(".timezone");
+let sel = document.createElement("select");
 sel.addEventListener("change", show);
-
 Object.keys(country).forEach(function(i) {
   var option = document.createElement("option");
   option.text = i;
   option.value = i;
   sel.appendChild(option);
 });
+ori.parentNode.insertBefore(sel, ori);
 
-ori.parentNode.insertBefore(sel,ori);
+//LT Queue Waiting Notification
+if (document.location.search.includes("GCARE_WEBTECH")){    
+const trigger = localStorage.getItem("myCat");
+  if(trigger){
+    console.log('working w/t Internet');
+
+    contains = (selector, text) => {
+      var elements = document.querySelectorAll(selector);
+      return Array.prototype.filter.call(elements, function(element){return RegExp(text).test(element.textContent); });
+    }
+    dimension = contains('.row','gCare WebTech')[0].innerText.replace(/sentiment_satisfied|sentiment_very_satisfied/g, '').split('\t');
+    VN_metric = contains('.row','Technical Solutions-VN-LT')[0].innerText.split('\t');
+    VN_metric.map(function(x){ return x.replace(/[^0-9\.:]+/g, "")});
   
+      var waiting =  VN_metric[1];
+      var wait_time = VN_metric[2];
+      var available = VN_metric[3];
+      var on_call = VN_metric[4];
+      var after_call = VN_metric[5];
+      var not_ready = VN_metric[6];
+  
+      var time_stamp = document.querySelector('[class="date"]').innerText;
+  
+        sessionStorage.setItem("waiting", waiting);
+        sessionStorage.setItem("wait_time", wait_time);
+        sessionStorage.setItem("available", available);
+        sessionStorage.setItem("on_call", on_call);
+        sessionStorage.setItem("after_call", after_call);
+        sessionStorage.setItem("not_ready", not_ready);
+        sessionStorage.setItem("time_stamp", time_stamp);
+  
+      
+      var url = 'https://chat.googleapis.com/v1/spaces/AAAANuXXRCc/messages?key=AIzaSyDdI0hCZtE6vySjMm-WEfRq3CPzqKqqsHI&token=22YYP7Spi9z-OQ4KYV8ZQ75F6k-QXOcmR6_jjkUSZY8';
+      
+      if (waiting !='0'){
+  
+        text = "waiting: " + waiting + "\n" + "available: " + available +  "on_call: " + on_call + " " + "after_call: " + after_call + " " + "not_ready: " + not_ready;
+        text.replace(/error/g, "");
+        const xhr = new XMLHttpRequest();
+        xhr.open("POST", url);
+        xhr.setRequestHeader("Content-Type", "application/json; charset=UTF-8");
+        const body = JSON.stringify({
+          text: text
+        });
+        xhr.onload = () => {
+          console.log(xhr)
+        }
+        xhr.send(body);
+      }
+  } else {
+    console.log('Check the value setting in localStorage');
+    //location.reload();
+        
+}}  
 })();
 
